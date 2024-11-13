03ff48930369514ba2614868db26ecc2067798f2
yerson333 <n00303696@upn.pe>
2024-11-13 13:07:37 -0500

    LOGIN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MeseroComponent } from './mesero/mesero.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MeseroComponent],
+  imports: [RouterOutlet, MeseroComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  iniciarSesion() {
+    if (this.username && this.password) {
+      // Implementar lógica de autenticación
+      alert(`Bienvenido, ${this.username}`);
+    } else {
+      alert('Por favor, ingresa tu usuario y contraseña.');
+    }
+  }
 }
